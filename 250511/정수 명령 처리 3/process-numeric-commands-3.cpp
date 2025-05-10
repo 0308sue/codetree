3c55cf2afcdfe291fd8c4264f2ffc700f9946f3a
0308sue <98823240+0308sue@users.noreply.github.com>
2025-05-11 02:18:33 +0900

    Title: 정수 명령 처리 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250511/정수 명령 처리 3/process-numeric-commands-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N;
+string cmd[10000];
+int num[10000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> cmd[i];
+        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
+            cin >> num[i];
+        }
+        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
+            cin >> num[i];
+        }
+        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
+            cin >> num[i];
+        }
+        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
+            cin >> num[i];
+        }
+        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
+            cin >> num[i];
+        }
+        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
+            cin >> num[i];
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
