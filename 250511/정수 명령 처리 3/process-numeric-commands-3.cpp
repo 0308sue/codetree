d32282172b96da8398f531a885f3c62ee215ef36
0308sue <98823240+0308sue@users.noreply.github.com>
2025-05-11 02:22:26 +0900

    Title: 정수 명령 처리 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250511/정수 명령 처리 3/process-numeric-commands-3.cpp
+++ 250511/정수 명령 처리 3/process-numeric-commands-3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <deque>
 using namespace std;
 
 int N;
@@ -8,26 +8,37 @@ string cmd[10000];
 int num[10000];
 
 int main() {
+    deque<int> dq;
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> cmd[i];
-        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
+        if (cmd[i] == "push_front") {
             cin >> num[i];
+            dq.push_front(num[i]);
         }
-        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
+        if (cmd[i] == "push_back") {
             cin >> num[i];
+            dq.push_back(num[i]);
         }
-        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
-            cin >> num[i];
+        if (cmd[i] == "pop_front") {
+            cout << dq.front() << endl;
+            dq.pop_front(); 
         }
-        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
-            cin >> num[i];
+        if (cmd[i] == "pop_back") {
+            cout << dq. back() << endl;
+            dq.pop_back(); 
         }
-        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
-            cin >> num[i];
+        if (cmd[i] == "size") {
+            cout << dq.size() << endl;
         }
-        if (cmd[i] == "push_front" || cmd[i] == "push_back") {
-            cin >> num[i];
+        if (cmd[i] == "empty") {
+            cout << dq.empty() << endl;
+        }
+        if (cmd[i] == "front") {
+            cout << dq.front() << endl;
+        }
+        if (cmd[i] == "back") {
+            cout << dq.back() << endl; 
         }
     }
 
