4ddb61bb79898bf1a956feb6f4387ec0faa2819d
0308sue <98823240+0308sue@users.noreply.github.com>
2025-05-11 01:17:22 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250511/괄호 문자열의 적합성 판단/parentheses-string.cpp
+++ 250511/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -13,7 +13,10 @@ int main() {
     for (char ch : str) {
         if (ch == '(') s.push('(');
         else{
-            if(s.empty() == 1) return false;
+            if(s.empty() == 1){
+                 cout << "No";
+                 return 0;
+            }
             s.pop();
         }
     }
@@ -23,16 +26,3 @@ int main() {
     return 0;
 }
 
-// function solution(str)
-//   set s = empty stack
-//   for i = 0 ... i < str.size  // 주어진 문자열을 순회합니다.
-//     if str[i] == '('          // 열린 괄호에 대해서는
-//       s.push('(')             // stack에 열린 괄호를 넣어줍니다.
-//     else
-//       if s.empty() == true    // 닫힌괄호가 나온 상황에서, stack이 비어있다면,
-//         return false          // 올바른 괄호 문자열이 아닙니다.
-//       s.pop()                 // 가장 위에 있는 값을 비워줍니다.
-
-//   if s.empty() == false       // 전부 순회했는데도 불구하고 stack이 비어있지 않다면
-//     return false              // 올바른 괄호 문자열이 아닙니다.
-//   return true                 // 전부 통과했다면 올바른 괄호 문자열 입니다.
